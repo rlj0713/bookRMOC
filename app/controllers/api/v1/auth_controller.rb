859fd033782c4231d001368464084500bb1aa196
Reid <rlj0713@gmail.com>
2021-11-06 15:40:52 -0400

    fetch works again

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -13,6 +13,24 @@ class Api::V1::AuthController < ApplicationController
     end
   end
 
+  # def login
+  #   user = User.find_by(username: params[:username])
+  #   if user && user.authenticate(params[:password])
+  #     payload = {user_id: user.id}
+  #     token = encode_token(payload)
+  #     render json: {user: user, jwt: token, success: "Welcome back, #{user.username}" }
+  #   else 
+  #     render json { errors: "Log in failed! Username or password invalid!" }
+  # end
+
+  # def auto_login
+  #   if current_user
+  #     render json: current_user
+  #   else
+  #     render json: {errors: "No User Logged In" }
+  #   end
+  # end
+
   private
 
   def user_login_params
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,11 +1,6 @@
 class Api::V1::UsersController < ApplicationController    
     skip_before_action :authorized, only: [:create]
 
-  def index
-    @users = User.all
-    render json: @users.to_json
-  end
-
   def profile
     render json: { user: UserSerializer.new(current_user) }, status: :accepted
   end
