b8bf30c1827e8f10750accd59e60b581ad473f4b
Reid <rlj0713@gmail.com>
2021-12-01 19:08:58 -0500

    refactored react authentication controller

--- app/controllers/google_controller.rb
+++ app/controllers/google_controller.rb
@@ -1,7 +1,6 @@
 class GoogleController < ApplicationController
 
     def create
-        byebug
         @user = User.find_or_create_by(username: auth['info']['email']) do |u|
             u.name = auth['info']['name']
             u.password = auth['uid'][-5...-1]
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,6 +11,10 @@ class SessionsController < ApplicationController
         end
     end
 
+    def authenticate
+        redirect_to '/auth/google/callback'
+    end
+
     def new
         @user = User.find_by_id(session[:user_id])
         if session[:user_id]
